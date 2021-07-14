2b84da85b83285b1115ea9f4244e1d54977d11ed
Abraomukas <abraomukas@gmail.com>
2021-07-14 11:12:05 +0400

    style for navbar links found

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,68 +9,66 @@ function Navbar(props) {
 	return (
 		<div>
 			<header>
-				<div className='px-3 text-white' style={backgroundColor}>
+				<div className='navbar navbar-expand-lg' style={backgroundColor}>
 					<div className='container-fluid'>
 						{/* Logo */}
-						<div className='d-flex navbar-brand justify-content-start'>
-							<Link to='/'>
+						<div className='d-flex justify-content-start align-items-center'>
+							<Link className='navbar-brand' to='/'>
 								<img
 									src={logoPath}
-									height='100'
+									height='75'
 									alt='easierSpeak'
 									loading='lazy'
 									style={logoStyle}
 								/>
-							</Link>{" "}
-							<Link className='navbar-brand' to='/'>
+							</Link>
+							<Link className='navbar-brand text-white' to='/'>
 								easierSpeak
 							</Link>
 						</div>
 						<div className='d-flex justify-content-end'>
 							<ul className='nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small'>
 								<li>
-									<Link to='#' className='nav-link text-secondary'>
-										<svg
-											className='bi d-block mx-auto mb-1'
-											width='24'
-											height='24'>
-											<use xlinkHref='#home'></use>
-										</svg>
-										Home
-									</Link>
+									<div className='mx-1 d-flex flex-column align-items-center'>
+										<Link to='#' className='nav-link'>
+											<img
+												src={logoPath}
+												height='30'
+												alt='easierSpeak'
+												loading='lazy'
+												style={logoStyle}
+											/>
+										</Link>
+										<p className='text-white'>Meetings</p>
+									</div>
 								</li>
 								<li>
-									<Link to='#' className='nav-link text-white'>
-										<svg
-											className='bi d-block mx-auto mb-1'
-											width='24'
-											height='24'>
-											<use xlinkHref='#speedometer2'></use>
-										</svg>
-										Meetings
-									</Link>
+									<div className='mx-1 d-flex flex-column align-items-center'>
+										<Link to='#' className='nav-link'>
+											<img
+												src={logoPath}
+												height='30'
+												alt='easierSpeak'
+												loading='lazy'
+												style={logoStyle}
+											/>
+										</Link>
+										<p className='text-white'>Clubs</p>
+									</div>
 								</li>
 								<li>
-									<Link to='#' className='nav-link text-white'>
-										<svg
-											className='bi d-block mx-auto mb-1'
-											width='24'
-											height='24'>
-											<use xlinkHref='#table'></use>
-										</svg>
-										Clubs
-									</Link>
-								</li>
-								<li>
-									<Link to='#' className='nav-link text-white'>
-										<svg
-											className='bi d-block mx-auto mb-1'
-											width='24'
-											height='24'>
-											<use xlinkHref='#grid'></use>
-										</svg>
-										FAQs
-									</Link>
+									<div className='mx-1 d-flex flex-column align-items-center'>
+										<Link to='#' className='nav-link'>
+											<img
+												src={logoPath}
+												height='30'
+												alt='easierSpeak'
+												loading='lazy'
+												style={logoStyle}
+											/>
+										</Link>
+										<p className='text-white'>FAQs</p>
+									</div>
 								</li>
 							</ul>
 						</div>
