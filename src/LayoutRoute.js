ebc9b6fe95286f935ec664aa9bd1595fa1b2d5de
Abraomukas <abraomukas@gmail.com>
2021-07-16 16:49:59 +0300

    added dropdown menus

--- src/LayoutRoute.js
+++ src/LayoutRoute.js
@@ -6,10 +6,22 @@ import { Route } from "react-router-dom";
  */
 import Navbar from "./components/Navbar";
 
+let headerSites = [
+	{ label: "Meetings", path: "/" },
+	{ label: "Clubs", path: "/" },
+	{ label: "Go to", path: "/" },
+	{ label: "FAQs", path: "/" },
+];
+
+const services = [
+	{ label: "Our doggos", path: "/our-doggos" },
+	{ label: "Our humans", path: "/our-humans" },
+];
+
 function LayoutRoute(props) {
 	return (
 		<div>
-			<Navbar />
+			<Navbar headerSites={headerSites} dropdown={services} />
 			<Route
 				path={props.path}
 				exact={props.exact}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,27 +4,21 @@ import { Link } from "react-router-dom";
 const backgroundColor = { backgroundColor: "rgb(0, 82, 122)" };
 
 const logoPath = "./images/tm-logo.png";
-const meetingsPath = "./images/meetings.png";
-const clubsPath = "./images/clubs.png";
-const faqsPath = "./images/faqs.png";
-
-const iconStyle = { marginTop: "2px" };
 
 function Navbar(props) {
 	return (
 		<div>
 			<header>
-				<div className='navbar' style={backgroundColor}>
+				<div className='navbar navbar-expand-lg' style={backgroundColor}>
 					<div className='container-fluid'>
 						{/* Brand */}
 						<div className='d-flex align-items-center'>
 							<Link className='navbar-brand' to='/'>
 								<img
 									src={logoPath}
-									height='75'
+									height='55'
 									alt='easierSpeak'
 									loading='lazy'
-									style={iconStyle}
 								/>
 							</Link>
 							<Link className='navbar-brand text-white' to='/'>
@@ -32,50 +26,44 @@ function Navbar(props) {
 							</Link>
 						</div>
 						{/* Sections */}
-						<ul className='nav'>
-							<li>
-								<div className='d-flex flex-column align-items-center'>
-									<Link to='#' className='nav-link'>
-										<img
-											src={meetingsPath}
-											height='40'
-											alt='Meetings'
-											loading='lazy'
-											style={iconStyle}
-										/>
+						<div className='collapse navbar-collapse'>
+							<ul className='navbar-nav'>
+								{props.headerSites.map((linkObj, index) => {
+									return (
+										<li key={index} className='nav-item'>
+											<Link className='nav-link active' to={linkObj.path}>
+												{linkObj.label}
+											</Link>
+										</li>
+									);
+								})}
+								<li className='nav-item dropdown'>
+									<Link
+										className='nav-link dropdown-toggle'
+										to=''
+										id='navbarDropdown'
+										role='button'
+										data-bs-toggle='dropdown'
+										aria-expanded='false'>
+										Our services
 									</Link>
-									<p className='text-white'>Meetings</p>
-								</div>
-							</li>
-							<li>
-								<div className='d-flex flex-column align-items-center'>
-									<Link to='#' className='nav-link'>
-										<img
-											src={clubsPath}
-											height='40'
-											alt='Clubs'
-											loading='lazy'
-											style={iconStyle}
-										/>
-									</Link>
-									<p className='text-white'>Clubs</p>
-								</div>
-							</li>
-							<li>
-								<div className='d-flex flex-column align-items-center'>
-									<Link to='#' className='nav-link'>
-										<img
-											src={faqsPath}
-											height='40'
-											alt='FAQs'
-											loading='lazy'
-											style={iconStyle}
-										/>
-									</Link>
-									<p className='text-white'>FAQs</p>
-								</div>
-							</li>
-						</ul>
+									<ul
+										className='dropdown-menu'
+										aria-labelledby='navbarDropdown'>
+										{props.dropdown.map((linkObj, index) => {
+											return (
+												<li key={index}>
+													<Link className='dropdown-item' to={linkObj.path}>
+														{linkObj.label}
+													</Link>
+												</li>
+											);
+										})}
+									</ul>
+								</li>
+							</ul>
+						</div>
+
 						{/* Profile */}
 						<Link className='btn btn-primary' to='/login' role='button'>
 							Login
