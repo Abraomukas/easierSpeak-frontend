38239470b98f43d1c9cad296baa1965d64928de8
Abraomukas <abraomukas@gmail.com>
2021-07-22 17:27:27 +0300

    Navbar components dissected

--- src/LayoutRoute.js
+++ src/LayoutRoute.js
@@ -6,22 +6,53 @@ import { Route } from "react-router-dom";
  */
 import Navbar from "./components/Navbar";
 
-let headerSites = [
-	{ label: "Meetings", path: "/" },
-	{ label: "Clubs", path: "/" },
-	{ label: "Go to", path: "/" },
-	{ label: "FAQs", path: "/" },
+const meetingsDropdown = [
+	{ label: "Create a meeting", path: "#" },
+	{ label: "Last meeting", path: "#" },
+	{ label: "Next meeting", path: "#" },
+	{ label: "Calendar", path: "#" },
+	{ label: "Meeting archive", path: "#" },
+	{ label: "All online meetings", path: "#" },
 ];
 
-const services = [
-	{ label: "Our doggos", path: "/our-doggos" },
-	{ label: "Our humans", path: "/our-humans" },
+const thisClubDropdown = [
+	{ label: "Home", path: "#" },
+	{ label: "Diretory", path: "#" },
+	{ label: "Officers / ExCo", path: "#" },
+	{ label: "Members", path: "#" },
+	// ? { label: "Charts", path: "#" },
+	{ label: "Speeches archive", path: "#" },
+	{ label: "Meetings archive", path: "#" },
+];
+
+const goToDropdown = [
+	{ label: "TI information", path: "#" },
+	{ label: "District stats", path: "#" },
+	{ label: "Map", path: "#" },
+	// ? { label: "Neighbouring clubs", path: "#" },
+	{ label: "Club search", path: "#" },
+	{ label: "Member search", path: "#" },
+];
+
+const helpDropdown = [
+	{ label: "FAQs", path: "#" },
+	{ label: "Discussion forum", path: "#" },
+	{ label: "Knowledge map", path: "#" },
+	{ label: "Site stats", path: "#" },
+	// ? { label: "Site groups", path: "#" },
+];
+
+const headerSites = [
+	{ label: "Meetings", dropdown: { meetingsDropdown } },
+	{ label: "This Club", dropdown: { thisClubDropdown } },
+	{ label: "Go to", dropdown: { goToDropdown } },
+	{ label: "FAQs", dropdown: { helpDropdown } },
 ];
 
 function LayoutRoute(props) {
 	return (
 		<div>
-			<Navbar headerSites={headerSites} dropdown={services} />
+			<Navbar headerSites={headerSites} />
 			<Route
 				path={props.path}
 				exact={props.exact}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import DropdownList from "./DropdownList";
 
 const backgroundColor = { backgroundColor: "rgb(0, 82, 122)" };
 
 const logoPath = "./images/tm-logo.png";
 
 function Navbar(props) {
+	const meetingsDropdown = props.headerSites[0].dropdown.meetingsDropdown;
+	const thisClubDropdown = props.headerSites[1].dropdown.thisClubDropdown;
+	const goToDropdown = props.headerSites[2].dropdown.goToDropdown;
+	const helpDropdown = props.headerSites[3].dropdown.helpDropdown;
+
+	const dropdowns = [
+		meetingsDropdown,
+		thisClubDropdown,
+		goToDropdown,
+		helpDropdown,
+	];
+
 	return (
 		<div>
 			<header>
@@ -25,44 +38,14 @@ function Navbar(props) {
 								easierSpeak
 							</Link>
 						</div>
-						{/* Sections */}
-						<div className='collapse navbar-collapse'>
-							<ul className='navbar-nav'>
-								{props.headerSites.map((linkObj, index) => {
-									return (
-										<li key={index} className='nav-item'>
-											<Link className='nav-link active' to={linkObj.path}>
-												{linkObj.label}
-											</Link>
-										</li>
-									);
-								})}
-								<li className='nav-item dropdown'>
-									<Link
-										className='nav-link dropdown-toggle'
-										to=''
-										id='navbarDropdown'
-										role='button'
-										data-bs-toggle='dropdown'
-										aria-expanded='false'>
-										Our services
-									</Link>
-									<ul
-										className='dropdown-menu'
-										aria-labelledby='navbarDropdown'>
-										{props.dropdown.map((linkObj, index) => {
-											return (
-												<li key={index}>
-													<Link className='dropdown-item' to={linkObj.path}>
-														{linkObj.label}
-													</Link>
-												</li>
-											);
-										})}
-									</ul>
-								</li>
-							</ul>
-						</div>
+						{/* Sections - Meetiings */}
+						<DropdownList label='Meetings' dropdown={meetingsDropdown} />
+						{/* Sections - This Club */}
+						<DropdownList label='This Club' dropdown={thisClubDropdown} />
+						{/* Sections - Go to */}
+						<DropdownList label='Go to' dropdown={goToDropdown} />
+						{/* Sections - FAQs */}
+						<DropdownList label='Help' dropdown={helpDropdown} />
 
 						{/* Profile */}
 						<Link className='btn btn-primary' to='/login' role='button'>
