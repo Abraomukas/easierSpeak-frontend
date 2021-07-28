9bbe0bbb4caa2bd05f10f9ed265e200834dbacc2
Abraomukas <abraomukas@gmail.com>
2021-07-28 21:44:58 +0300

    Profile info section of the Register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -52,7 +52,7 @@ function Register() {
 						type='password'
 						className='form-control'
 						id='floatingPassword'
-						placeholder='Password'
+						placeholder='password'
 					/>
 					<Translation>
 						{(t) => (
@@ -68,7 +68,7 @@ function Register() {
 						type='password'
 						className='form-control'
 						id='floatingPassword'
-						placeholder='Password'
+						placeholder='password'
 					/>
 					<Translation>
 						{(t) => (
@@ -78,8 +78,102 @@ function Register() {
 						)}
 					</Translation>
 				</div>
-				<div></div>
-				{/* Profile information */}
+				<div className='mt-3'>
+					{/* Profile information */}
+					{/* Title */}
+					<div className='text-center'>
+						<Translation>
+							{(t) => (
+								<h1 className='h3 mb-4 fw-normal'>
+									{t("page_register.profile_info")}
+								</h1>
+							)}
+						</Translation>
+					</div>
+					{/* First name */}
+					<div className='form-floating'>
+						<input
+							type='firstName'
+							className='form-control'
+							id='floatingInput'
+							placeholder='first name'
+						/>
+						<Translation>
+							{(t) => (
+								<label htmlFor='floatingInput'>
+									{t("page_register.first_name")}
+								</label>
+							)}
+						</Translation>
+					</div>
+					{/* Last name */}
+					<div className='form-floating'>
+						<input
+							type='lastName'
+							className='form-control'
+							id='floatingInput'
+							placeholder='last name'
+						/>
+						<Translation>
+							{(t) => (
+								<label htmlFor='floatingInput'>
+									{t("page_register.last_name")}
+								</label>
+							)}
+						</Translation>
+					</div>
+					{/* Name visibility */}
+					<div className='mt-3'>
+						<Translation>
+							{(t) => (
+								<label htmlFor='floatingInput'>
+									{t("page_register.name_visibility.title")}
+								</label>
+							)}
+						</Translation>
+						<div className='mt-3'>
+							<div class='form-check form-check-inline'>
+								<input
+									class='form-check-input'
+									type='radio'
+									name='inlineRadioOptions'
+									id='inlineRadio1'
+									value='option1'
+								/>
+								<label class='form-check-label' for='inlineRadio1'>
+									1
+								</label>
+							</div>
+							<div class='form-check form-check-inline'>
+								<input
+									class='form-check-input'
+									type='radio'
+									name='inlineRadioOptions'
+									id='inlineRadio2'
+									value='option2'
+								/>
+								<label class='form-check-label' for='inlineRadio2'>
+									2
+								</label>
+							</div>
+							<div class='form-check form-check-inline'>
+								<input
+									class='form-check-input'
+									type='radio'
+									name='inlineRadioOptions'
+									id='inlineRadio3'
+									value='option3'
+								/>
+								<label class='form-check-label' for='inlineRadio3'>
+									3
+								</label>
+							</div>
+						</div>
+					</div>
+					{/* Location */}
+					{/* Phone number */}
+					{/* Phone number visibility */}
+				</div>
 				{/* General preferences */}
 			</div>
 		</div>
