26606b6427526a5eedee377cd01cb62269897e13
Abraomukas <abraomukas@gmail.com>
2021-08-04 17:49:28 +0300

    App wrapped in a Provider that uses the store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import "./i18n";
 
 import App from "./App";
-import "./i18n";
+import store from "./store";
 
 ReactDOM.render(
 	<React.Suspense fallback='loading'>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.Suspense>,
 	document.getElementById("root")
 );
---
+++ src/store.js
@@ -0,0 +1,20 @@
+import { createStore } from "redux";
+import { combineReducers } from "redux";
+
+/*
+ * Reducers
+ */
+import loginReducer from "./reducers/login";
+import languageReducer from "./reducers/language";
+
+const allReducers = combineReducers({
+	login: loginReducer,
+	language: languageReducer,
+});
+
+const store = createStore(
+	allReducers,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
