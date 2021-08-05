03c6f5b212f24e287fc99f266738e7a69f950c23
Abraomukas <abraomukas@gmail.com>
2021-08-05 21:58:54 +0300

    added fallback with a Spinner to the App's Suspense

--- src/index.js
+++ src/index.js
@@ -7,8 +7,17 @@ import "./i18n";
 import App from "./App";
 import store from "./store";
 
+const spinnerSizing = { width: "3rem", height: "3rem" };
+const spinnerFallback = (
+	<div className='d-flex justify-content-center mt-3'>
+		<div class='spinner-border' style={spinnerSizing} role='status'>
+			<span class='visually-hidden'>Loading...</span>
+		</div>
+	</div>
+);
+
 ReactDOM.render(
-	<React.Suspense fallback='loading'>
+	<React.Suspense fallback={spinnerFallback}>
 		<Provider store={store}>
 			<App />
 		</Provider>
