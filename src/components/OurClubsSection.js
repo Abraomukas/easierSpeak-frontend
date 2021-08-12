817cf69f8a88721db78758781fdfeb0bcefd9912
Abraomukas <abraomukas@gmail.com>
2021-08-12 14:20:56 +0300

    super duper cool doughnut chart added

--- src/components/OurClubsSection.js
+++ src/components/OurClubsSection.js
@@ -1,5 +1,16 @@
 import React from "react";
 import { Translation } from "react-i18next";
+import { Doughnut } from "react-chartjs-2";
+
+const data = {
+	labels: ["Community Clubs", "Corporate Clubs"],
+	datasets: [
+		{
+			data: [20, 5],
+			backgroundColor: ["#8d323f", "#235679"],
+		},
+	],
+};
 
 function OurClubsSection() {
 	return (
@@ -9,7 +20,14 @@ function OurClubsSection() {
 					{(t) => <h2>{t("page_main.headlines.our_clubs.title")}</h2>}
 				</Translation>
 			</div>
-			<div></div>
+			<div>
+				<Doughnut
+					data={data}
+					width={"300"}
+					height={"300"}
+					options={{ maintainAspectRatio: false }}
+				/>
+			</div>
 		</section>
 	);
 }
