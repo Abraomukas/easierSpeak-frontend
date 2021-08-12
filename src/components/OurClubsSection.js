10fdf074fc20de26717554695f280f788d1c0eab
Abraomukas <abraomukas@gmail.com>
2021-08-12 16:00:30 +0300

    translations added

--- src/components/OurClubsSection.js
+++ src/components/OurClubsSection.js
@@ -1,18 +1,23 @@
 import React from "react";
-import { Translation } from "react-i18next";
+import { Translation, useTranslation } from "react-i18next";
 import { Doughnut } from "react-chartjs-2";
 
-const data = {
-	labels: ["Community Clubs", "Corporate Clubs"],
-	datasets: [
-		{
-			data: [20, 5],
-			backgroundColor: ["#8d323f", "#235679"],
-		},
-	],
-};
-
 function OurClubsSection() {
+	const { t } = useTranslation();
+
+	const data = {
+		labels: [
+			t("page_main.headlines.our_clubs.chart.community_clubs"),
+			t("page_main.headlines.our_clubs.chart.corporate_clubs"),
+		],
+		datasets: [
+			{
+				data: [20, 5],
+				backgroundColor: ["#8d323f", "#235679"],
+			},
+		],
+	};
+
 	return (
 		<section>
 			<div className='d-flex flex-row justify-content-center my-3'>
