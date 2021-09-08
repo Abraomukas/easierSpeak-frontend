7a6d1c0d01a8eb6cb5657815ac587165c1660eb9
Abraomukas <abraomukas@gmail.com>
2021-09-08 15:44:07 +0400

    skeleton of the page

---
+++ src/pages/MyClub.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MyClub() {
+	return (
+		<div>
+			{/* TITLE */}
+			{/* ABOUT */}
+			{/* CLUB DETAILS */}
+			{/* TM MISSION */}
+			{/* TM PITCH */}
+			{/* EXCO */}
+			{/* ALL MEMBERS */}
+			{/* SOCIAL */}
+			<div></div>
+		</div>
+	);
+}
+
+export default MyClub;
