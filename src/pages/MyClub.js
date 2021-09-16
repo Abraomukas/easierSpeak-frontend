539a643e0205de5b8a7fa0883fffae68c52ce9bd
Abraomukas <abraomukas@gmail.com>
2021-09-16 16:39:48 +0400

    using the Grid layout

--- src/pages/MyClub.js
+++ src/pages/MyClub.js
@@ -7,47 +7,59 @@ function MyClub() {
 			{/* TITLE */}
 			<section>
 				<div
-					className='d-flex justify-content-center'
+					className='d-flex justify-content-center mb-3'
 					style={{ marginTop: "7rem" }}>
 					<h1>Skylarks Toastmasters Club</h1>
 				</div>
 			</section>
 
-			{/* ABOUT */}
 			<section>
-				<div className='my-3'>
-					<Translation>{(t) => <h3>{t("my_club.about")}</h3>}</Translation>
-					<Translation>
-						{(t) => (
-							<p>
-								{t("my_club.welcome")}
-								<strong>{t("my_club.env")}</strong>
-							</p>
-						)}
-					</Translation>
-					<Translation>
-						{(t) => <p>{t("my_club.queries.title")}</p>}
-					</Translation>
-					<ul>
-						<li>
+				<div className='container my-3'>
+					<div className='row'>
+						{/* ABOUT */}
+						<div className='col'>
+							<div className='text-center'>
+								<Translation>
+									{(t) => <h3>{t("my_club.about")}</h3>}
+								</Translation>
+							</div>
 							<Translation>
-								{(t) => <p>{t("my_club.queries.q1")}</p>}
+								{(t) => (
+									<p>
+										{t("my_club.welcome")}
+										<strong>{t("my_club.env")}</strong>
+									</p>
+								)}
 							</Translation>
-						</li>
-						<li>
 							<Translation>
-								{(t) => <p>{t("my_club.queries.q2")}</p>}
+								{(t) => <p>{t("my_club.queries.title")}</p>}
 							</Translation>
-						</li>
-						<li>
+							<ul>
+								<li>
+									<Translation>
+										{(t) => <p>{t("my_club.queries.q1")}</p>}
+									</Translation>
+								</li>
+								<li>
+									<Translation>
+										{(t) => <p>{t("my_club.queries.q2")}</p>}
+									</Translation>
+								</li>
+								<li>
+									<Translation>
+										{(t) => <p>{t("my_club.queries.q3")}</p>}
+									</Translation>
+								</li>
+							</ul>
 							<Translation>
-								{(t) => <p>{t("my_club.queries.q3")}</p>}
+								{(t) => <p>{t("my_club.queries.statement")}</p>}
 							</Translation>
-						</li>
-					</ul>
-					<Translation>
-						{(t) => <p>{t("my_club.queries.statement")}</p>}
-					</Translation>
+						</div>
+						{/* ExCo */}
+						<div className='col text-center'>
+							<Translation>{(t) => <h3>{t("my_club.about")}</h3>}</Translation>
+						</div>
+					</div>
 				</div>
 			</section>
 
@@ -55,12 +67,12 @@ function MyClub() {
 			{/* TM MISSION */}
 			{/* TM PITCH */}
 			<section>
-				<div className='mh-100'></div>
+				<div className='my-3'></div>
 			</section>
 
 			{/* SOCIAL */}
 			<section>
-				<div className='mh-100'></div>
+				<div className='my-3'></div>
 			</section>
 		</div>
 	);
