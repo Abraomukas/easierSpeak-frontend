c32cae644cb337a6dc49ed0e145e641bf7e4b8f3
Abraomukas <abraomukas@gmail.com>
2021-10-02 18:31:36 +0400

    new layout

--- src/pages/MyClub.js
+++ src/pages/MyClub.js
@@ -21,6 +21,12 @@ function MyClub() {
 			<section>
 				<div className='container my-3'>
 					<div className='row'>
+						{/* TM MISSION */}
+						<div className='col'></div>
+						{/* TM PITCH */}
+						<div className='col'></div>
+					</div>
+					<div className='row'>
 						{/* ABOUT */}
 						<div className='col'>
 							<div className='text-center'>
@@ -60,172 +66,172 @@ function MyClub() {
 								{(t) => <p>{t("my_club.queries.statement")}</p>}
 							</Translation>
 						</div>
-
-						{/* ExCo */}
-						<div className='col text-center'>
-							<div>
-								<Translation>{(t) => <h3>{t("my_club.exco")}</h3>}</Translation>
-							</div>
-							<div>
-								<table class='table'>
-									<tbody>
-										<tr>
-											<th scope='col'>President</th>
-											<td>
-												<img
-													src={excoPic}
-													height='55'
-													alt='Profile pic'
-													loading='lazy'
-												/>
-											</td>
-											<td>Gabor Fekete</td>
-											<td>
-												<button>Contact</button>
-											</td>
-										</tr>
-										<tr>
-											<th scope='row'>Vice President of Education</th>
-											<td>
-												<img
-													src={excoPic}
-													height='55'
-													alt='Profile pic'
-													loading='lazy'
-												/>
-											</td>
-											<td>Abraham Hernández Valencia</td>
-											<td>
-												<button>Contact</button>
-											</td>
-										</tr>
-										<tr>
-											<th scope='row'>Vice President of Membership</th>
-											<td>
-												<img
-													src={excoPic}
-													height='55'
-													alt='Profile pic'
-													loading='lazy'
-												/>
-											</td>
-											<td>Bence Ács</td>
-											<td>
-												<button>Contact</button>
-											</td>
-										</tr>
-										<tr>
-											<th scope='row'>Vice President of Public Relations</th>
-											<td>
-												<img
-													src={excoPic}
-													height='55'
-													alt='Profile pic'
-													loading='lazy'
-												/>
-											</td>
-											<td>Judit Saad</td>
-											<td>
-												<button>Contact</button>
-											</td>
-										</tr>
-										<tr>
-											<th scope='row'>Vice President of Mentorship</th>
-											<td>
-												<img
-													src={excoPic}
-													height='55'
-													alt='Profile pic'
-													loading='lazy'
-												/>
-											</td>
-											<td>Klaudia Fodor</td>
-											<td>
-												<button>Contact</button>
-											</td>
-										</tr>
-										<tr>
-											<th scope='row'>Secretary</th>
-											<td>
-												<img
-													src={excoPic}
-													height='55'
-													alt='Profile pic'
-													loading='lazy'
-												/>
-											</td>
-											<td>Kata Dömötör</td>
-											<td>
-												<button>Contact</button>
-											</td>
-										</tr>
-										<tr>
-											<th scope='row'>Treasurer</th>
-											<td>
-												<img
-													src={excoPic}
-													height='55'
-													alt='Profile pic'
-													loading='lazy'
-												/>
-											</td>
-											<td>Zsolt Plázár</td>
-											<td>
-												<button>Contact</button>
-											</td>
-										</tr>
-										<tr>
-											<th scope='row'>Sergeant at Arms</th>
-											<td>
-												<img
-													src={excoPic}
-													height='55'
-													alt='Profile pic'
-													loading='lazy'
-												/>
-											</td>
-											<td>Henry Scullion</td>
-											<td>
-												<button>Contact</button>
-											</td>
-										</tr>
-										<tr>
-											<th scope='row'>IT Support</th>
-											<td>
-												<img
-													src={excoPic}
-													height='55'
-													alt='Profile pic'
-													loading='lazy'
-												/>
-											</td>
-											<td>Henry Scullion</td>
-											<td>
-												<button>Contact</button>
-											</td>
-										</tr>
-										<tr>
-											<th scope='row'>Immediate Past President</th>
-											<td>
-												<img
-													src={excoPic}
-													height='55'
-													alt='Profile pic'
-													loading='lazy'
-												/>
-											</td>
-											<td>Eszter Nagy</td>
-											<td>
-												<button>Contact</button>
-											</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						</div>
 					</div>
 				</div>
 			</section>
+
+			{/* ExCo */}
+			<div className='col text-center'>
+				<div>
+					<Translation>{(t) => <h3>{t("my_club.exco")}</h3>}</Translation>
+				</div>
+				<div>
+					<table class='table'>
+						<tbody>
+							<tr>
+								<th scope='col'>President</th>
+								<td>
+									<img
+										src={excoPic}
+										height='55'
+										alt='Profile pic'
+										loading='lazy'
+									/>
+								</td>
+								<td>Gabor Fekete</td>
+								<td>
+									<button>Contact</button>
+								</td>
+							</tr>
+							<tr>
+								<th scope='row'>Vice President of Education</th>
+								<td>
+									<img
+										src={excoPic}
+										height='55'
+										alt='Profile pic'
+										loading='lazy'
+									/>
+								</td>
+								<td>Abraham Hernández Valencia</td>
+								<td>
+									<button>Contact</button>
+								</td>
+							</tr>
+							<tr>
+								<th scope='row'>Vice President of Membership</th>
+								<td>
+									<img
+										src={excoPic}
+										height='55'
+										alt='Profile pic'
+										loading='lazy'
+									/>
+								</td>
+								<td>Bence Ács</td>
+								<td>
+									<button>Contact</button>
+								</td>
+							</tr>
+							<tr>
+								<th scope='row'>Vice President of Public Relations</th>
+								<td>
+									<img
+										src={excoPic}
+										height='55'
+										alt='Profile pic'
+										loading='lazy'
+									/>
+								</td>
+								<td>Judit Saad</td>
+								<td>
+									<button>Contact</button>
+								</td>
+							</tr>
+							<tr>
+								<th scope='row'>Vice President of Mentorship</th>
+								<td>
+									<img
+										src={excoPic}
+										height='55'
+										alt='Profile pic'
+										loading='lazy'
+									/>
+								</td>
+								<td>Klaudia Fodor</td>
+								<td>
+									<button>Contact</button>
+								</td>
+							</tr>
+							<tr>
+								<th scope='row'>Secretary</th>
+								<td>
+									<img
+										src={excoPic}
+										height='55'
+										alt='Profile pic'
+										loading='lazy'
+									/>
+								</td>
+								<td>Kata Dömötör</td>
+								<td>
+									<button>Contact</button>
+								</td>
+							</tr>
+							<tr>
+								<th scope='row'>Treasurer</th>
+								<td>
+									<img
+										src={excoPic}
+										height='55'
+										alt='Profile pic'
+										loading='lazy'
+									/>
+								</td>
+								<td>Zsolt Plázár</td>
+								<td>
+									<button>Contact</button>
+								</td>
+							</tr>
+							<tr>
+								<th scope='row'>Sergeant at Arms</th>
+								<td>
+									<img
+										src={excoPic}
+										height='55'
+										alt='Profile pic'
+										loading='lazy'
+									/>
+								</td>
+								<td>Henry Scullion</td>
+								<td>
+									<button>Contact</button>
+								</td>
+							</tr>
+							<tr>
+								<th scope='row'>IT Support</th>
+								<td>
+									<img
+										src={excoPic}
+										height='55'
+										alt='Profile pic'
+										loading='lazy'
+									/>
+								</td>
+								<td>Henry Scullion</td>
+								<td>
+									<button>Contact</button>
+								</td>
+							</tr>
+							<tr>
+								<th scope='row'>Immediate Past President</th>
+								<td>
+									<img
+										src={excoPic}
+										height='55'
+										alt='Profile pic'
+										loading='lazy'
+									/>
+								</td>
+								<td>Eszter Nagy</td>
+								<td>
+									<button>Contact</button>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
 
 			<section>
 				<div className='container my-3'>
